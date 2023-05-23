744ab5ba7c723e7b43aacb1147b66d629da038c7
xiaoqingxx <2783123570@qq.com>
2023-05-23 16:38:08 +0800

    Initial commit

---
+++ src/test/java/com/qinglong/mynetdisk/MyNetdiskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qinglong.mynetdisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyNetdiskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
